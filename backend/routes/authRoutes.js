98ec461a79d1399e1f9cdc3bcacdf1248552d268
Negin | 기긴 <callmegigin@gmail.com>
2026-06-16 18:54:26 +0330

    Added Routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,11 +4,15 @@ const middleware = require("../middleware/authMiddleware");
 const {
   newUser,
   loginUser,
+  forgotPassword,
+  resetPassword,
   getUserProfile,
 } = require("../controllers/authController");
 
 router.post("/register", newUser);
 router.post("/login", loginUser);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
 router.get("/profile", middleware.protect, getUserProfile);
 
 module.exports = router;
